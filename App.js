d2ea0cf8be6ded894c275df3cda7ee1e4bf51877
unknown <gabrielfariaw@gmail.com>
2025-09-08 16:57:30 -0300

    Login screen layout and logic

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [email, setEmail] = useState('');
+  const[password, setPassword] = useState('');
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <View style={StyleSheet.container}>
+      <Text style={StyleSheet.title}>Login</Text>
+
+      <View style={StyleSheet.inputBox}>
+        <TextInput 
+          style={StyleSheet.input} 
+          placeholder="Email" 
+          placeholderTextColor={"#8be9fd"}
+          value={email} 
+          onChangeText={setEmail} 
+        />
+      </View>
+      <View style={StyleSheet.inputBox}>
+        <TextInput 
+          style={StyleSheet.input} 
+          placeholder="Passwrd" 
+          placeholderTextColor={"#8be9fd"}
+          secureTextEntry
+          value={password} 
+          onChangeText={setPassword} 
+        />
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Log in</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.link}>Forgot password?</Text>
+      
+      <Text style={styles.signup}>
+        Don't have an account? <Text style={styles.signupBold}>Sign up</Text>
+      </Text>
+    </View>
+  )
+}
