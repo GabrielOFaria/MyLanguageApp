cb5429c1fc7ed311072b0a3d7169986c3e84600c
unknown <gabrielfariaw@gmail.com>
2025-09-08 17:35:41 -0300

    Add login screen with styles

--- App.js
+++ App.js
@@ -5,22 +5,22 @@ export default function App() {
   const[password, setPassword] = useState('');
 
   return (
-    <View style={StyleSheet.container}>
-      <Text style={StyleSheet.title}>Login</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
 
-      <View style={StyleSheet.inputBox}>
+      <View style={styles.inputBox}>
         <TextInput 
-          style={StyleSheet.input} 
+          style={styles.input} 
           placeholder="Email" 
           placeholderTextColor={"#8be9fd"}
           value={email} 
           onChangeText={setEmail} 
         />
       </View>
-      <View style={StyleSheet.inputBox}>
+      <View style={styles.inputBox}>
         <TextInput 
-          style={StyleSheet.input} 
-          placeholder="Passwrd" 
+          style={styles.input} 
+          placeholder="Password" 
           placeholderTextColor={"#8be9fd"}
           secureTextEntry
           value={password} 
@@ -39,3 +39,61 @@ export default function App() {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0f1624',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 40,
+    fontWeight: 'bold',
+    marginBottom: 40,
+  },
+  inputBox: {
+    width: '100%',
+    backgroundColor: '#1a2236',
+    borderRadius: 20,
+    marginBottom: 20,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    boderWidth: 1,
+    borderColor: '#00bfff',
+  },
+  input: {
+    color: '#fff',
+    fontSize: 18,
+  },
+  button: {
+    width: '100%',
+    backgroundColor: '#00bfff',
+    borderRadius: 20,
+    padding: 15,
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  buttonText:{
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+
+  link: {
+    color: '#8be9fd',
+    marginBottom: 30,    
+  },
+  signup: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  signupBold: {
+    fontWeight: 'bold',
+    color: '#fff',
+    fontSize: 18,
+    },
+  });
