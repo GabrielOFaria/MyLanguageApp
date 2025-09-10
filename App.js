c7361ac9b2f395cbbb3be73ab432198b02d124df
unknown <gabrielfariaw@gmail.com>
2025-09-10 15:51:40 -0300

    style:applied new styles to login screen

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import * as Animatable from "react-native-animatable";
+import { useWindowDimensions } from "react-native";
 import {
   KeyboardAvoidingView,
   Platform,
@@ -9,150 +11,193 @@ import {
   TouchableOpacity,
   StyleSheet,
   TouchableWithoutFeedback,
-  Keyboard
-} from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+  Keyboard,
+} from "react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 export default function App() {
-  const [email, setEmail] = useState('');
-  const[password, setPassword] = useState('');
-  const [passwordVisible, setPasswordVisible] = useState(false); 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const handleLogin = () => {
+    if (!email.trim()) {
+      alert("Please enter your email");
+      return;
+    }
+    if (!password.trim()) {
+      alert("Please enter your password");
+      return;
+    }
+    alert("Login Sucessful"); // Handle login logic here
+  };
 
   return (
     <KeyboardAvoidingView
-  style={{ flex: 1 }}
-  behavior={Platform.OS === "ios" ? "padding" : "height"}
->
-    <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss();}}>
-  <ScrollView
-    contentContainerStyle={{ flexGrow: 1 }}
-    keyboardShouldPersistTaps="handled"
-  >
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <TouchableWithoutFeedback
+        onPress={() => {
+          Keyboard.dismiss();
+        }}
+      >
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <View style={styles.container}>
+            <Text style={styles.title}>Welcome Back</Text>
+            <Animatable.View
+              animation="fadeInUp"
+              duration={1000}
+              style={styles.inputBox}
+            >
+              <Icon
+                name="email-outline"
+                size={24}
+                color="#00bfff"
+                style={{ marginRight: 10 }}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Email"
+                placeholderTextColor={"#8be9fd"}
+                value={email}
+                onChangeText={setEmail}
+              />
+            </Animatable.View>
+            <Animatable.View
+              animation="fadeInUp"
+              delay={200}
+              duration={1000}
+              style={styles.inputBox}
+            >
+              <View
+                style={{ flexDirection: "row", alignItems: "center", flex: 1 }}
+              >
+                <Icon
+                  name="lock-outline"
+                  size={24}
+                  color="#00bfff"
+                  style={{ marginRight: 10 }}
+                />
+                <TextInput
+                  style={[styles.input, { flex: 1 }]}
+                  placeholder="Password"
+                  placeholderTextColor="#8be9fd"
+                  secureTextEntry={!passwordVisible}
+                  value={password}
+                  onChangeText={setPassword}
+                />
+              </View>
 
-      <View style={styles.container}>
-      <View style={styles.inputBox}>
-        <Icon name="email-outline" size={24} color="#00bfff" style={{ marginRight: 10 }} />
-        <TextInput 
-          style={styles.input} 
-          placeholder="Email" 
-          placeholderTextColor={"#8be9fd"}
-          value={email} 
-          onChangeText={setEmail} 
-        />
-      </View>    
-      <View style={styles.inputBox}>
-  <View style={{ flexDirection: 'row', alignItems: 'center', flex: 1 }}>
-    <Icon
-      name="lock-outline"
-      size={24}
-      color="#00bfff"
-      style={{ marginRight: 10 }}
-    />
-    <TextInput
-      style={[styles.input, { flex: 1 }]}
-      placeholder="Password"
-      placeholderTextColor="#8be9fd"
-      secureTextEntry={!passwordVisible}
-      value={password}
-      onChangeText={setPassword}
-    />
-  </View>
+              {/* Eye */}
+              <TouchableOpacity
+                onPress={() => setPasswordVisible(!passwordVisible)}
+              >
+                <Icon
+                  name={passwordVisible ? "eye-off-outline" : "eye-outline"}
+                  size={24}
+                  color="#00bfff"
+                />
+              </TouchableOpacity>
+            </Animatable.View>
 
-  {/* Eye */}
-  <TouchableOpacity onPress={() => setPasswordVisible(!passwordVisible)}>
-    <Icon
-      name={passwordVisible ? 'eye-off-outline' : 'eye-outline'}
-      size={24}
-      color="#00bfff"
-    />
-  </TouchableOpacity>
-</View>
+            <Animatable.View
+              animation="fadeInUp"
+              delay={400}
+              duration={1000}
+              style={{ alignItems: "center" }}
+            >
+              <TouchableOpacity style={styles.button} onPress={handleLogin}>
+                <Text style={styles.buttonText}>Log in</Text>
+              </TouchableOpacity>
+            </Animatable.View>
 
-      
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Log in</Text>
-      </TouchableOpacity>
+            <Text style={styles.link}>Forgot password?</Text>
 
-      <Text style={styles.link}>Forgot password?</Text>
-      
-      <Text style={styles.signup}>
-        Don't have an account? <Text style={styles.signupBold}>Sign up</Text>
-      </Text>
-    </View>
-  </ScrollView>
-  </TouchableWithoutFeedback>
-</KeyboardAvoidingView>
-  )
+            <Text style={styles.signup}>
+              Don't have an account?{" "}
+              <Text style={styles.signupBold}>Sign up</Text>
+            </Text>
+          </View>
+        </ScrollView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#0f1624',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#0f1624",
+    alignItems: "stretch",
+    justifyContent: "center",
     padding: 20,
   },
   title: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 40,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 40,
+    textAlign: "center",
+    alignSelf: "center",
   },
   inputBox: {
-    width: '100%',
-    backgroundColor: '#1a2236',
+    width: "100%",
+    backgroundColor: "#1a2236",
     borderRadius: 20,
     marginBottom: 20,
     paddingHorizontal: 20,
     paddingVertical: 10,
     borderWidth: 2,
-    borderColor: '#00bfff',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    shadowColor: '#00bfff',
+    borderColor: "#00bfff",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    shadowColor: "#00bfff",
     shadowOffset: { width: 0, height: 0 },
     shadowOpacity: 1,
     shadowRadius: 25,
-    
   },
   input: {
     flex: 1,
-    color: '#fff',
+    color: "#fff",
     fontSize: 18,
   },
   button: {
-    width: '100%',
-    backgroundColor: '#00bfff',
+    width: "75%",
+    alignSelf: "center",
+    backgroundColor: "#00bfff",
     borderRadius: 20,
-    padding: 15,
-    alignItems: 'center',
+    paddingVertical: 18,
+    paddingHorizontal: 30,
+    alignItems: "center",
     marginTop: 10,
     marginBottom: 20,
-    shadowColor: '#00bfff',
+    shadowColor: "#00bfff",
     shadowOffset: { width: 0, height: 0 },
     shadowOpacity: 1,
     shadowRadius: 25,
-    
   },
-  buttonText:{
-    color: '#fff',
+  buttonText: {
+    color: "#fff",
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 
   link: {
-    color: '#8be9fd',
-    marginBottom: 30,    
+    color: "#8be9fd",
+    marginBottom: 30,
   },
   signup: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 16,
   },
   signupBold: {
-    fontWeight: 'bold',
-    color: '#fff',
+    fontWeight: "bold",
+    color: "#fff",
     fontSize: 18,
-    },
-  });
+  },
+});
